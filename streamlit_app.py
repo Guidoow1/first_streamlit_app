4da77f00796e265fad8958c6f2a00be6cbd27a03
Guidoow1 <151166010+Guidoow1@users.noreply.github.com>
2023-11-17 13:46:28 +0100

    Update streamlit_app.py
    Copy pasted snowflake tekst

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.text('This is a test to see if I am doing it right')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
